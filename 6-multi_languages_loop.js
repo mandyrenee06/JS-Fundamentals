037a6618b960ca7f256edb43dcc48e658791f645
Mandy Renee <amandarenee195@gmail.com>
2025-11-14 07:05:26 +0300

    Add files via upload
    Add 6-multi_languages_loop.js task

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,14 @@
+const messages = [
+  "C is fun",
+  "Python is cool", 
+  "JavaScript is amazing"
+];
+
+let output = '';
+
+for (let i = 0; i < messages.length; i++) {
+  
+  output += messages[i] + '\n';
+}
+
+console.log(output);
